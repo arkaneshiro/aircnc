b5cb3dd31785956b9b435fe60be5e1506ee50da2
Jesse Solis <jesse.solis0316@gmail.com>
2020-04-24 16:09:22 -0500

    restructured kitchen-form started on booking form

--- frontend/index.js
+++ frontend/index.js
@@ -13,29 +13,36 @@ app.get('/', (req, res) => {
 
 app.get('/listings', (req, res) => {
         res.render('listingMain');
-    })
+})
 
 app.get('/kitchens/create', (req, res) => {
-    res.render('createKitchen');
-});
+        res.render('createKitchen');
+})
 
 app.get('/profile', (req, res) => {
-    res.render('profile')
-});
+                res.render('profile')
+})
 
 app.get('/bookings/(:id(\\d+))', (req, res) => {
-    // localStorage.setItem("AIRCNC_CURRENT_BOOKING", req.params.id)
-    res.render('bookings')
-});
+                // localStorage.setItem("AIRCNC_CURRENT_BOOKING", req.params.id)
+        er('bookings')
+})
 
-/*******************************************
- *  Route '/kitchen/:id'
- *      GET endpoint
- *          - renders kitchen details page
- *          - API call to GET '/kitchen/:id'
- *******************************************/
+// ****************************************
+//      *  Route '/kitchen/:id'
+//      *      GET endpoint
+//      *          - renders kitchen details page
+//      *          - API call to GET '/kitchen/:id'
+//      *******************************************/
+
+
 app.get('/kitchen/:id(\\d+)', (req, res) => {
-    res.render('kitchen-details');
-});
+        res.render('kitchen-details');
+})
+
+app.get('/listings/:id(\\d+)/checkout', (req, res) => {
+    res.render('listing-checkout');
+            });
+        
 
 app.listen(4000, () => console.log(`Listening on port 4000...`));
