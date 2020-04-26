12ae20000578dcb1e03357a5193adb5897ece8de
Riki Kaneshiro <arkaneshiro@gmail.com>
2020-04-25 19:11:18 -0700

    changed link in profile to be a 'cancel booking button' and changed bookings/id to cancel booking when DOMContentLoaded

--- frontend/public/scripts/profile.js
+++ frontend/public/scripts/profile.js
@@ -7,9 +7,12 @@ if (localStorage.getItem("AIRCNC_ACCESS_TOKEN") === null) {
 }
 
 logout.addEventListener("click", () => {
+    event.preventDefault()
     localStorage.removeItem("AIRCNC_ACCESS_TOKEN");
     localStorage.removeItem("AIRCNC_CURRENT_USER_ID");
     localStorage.removeItem("AIRCNC_CURRENT_USER_ROLE");
+    window.location.href = "/";
+    return;
 })
 
 document.addEventListener("DOMContentLoaded", async () => {
@@ -28,7 +31,6 @@ document.addEventListener("DOMContentLoaded", async () => {
         }
 
         const { guestBookings } = await res.json();
-        // console.log(guestBookings);
         const pastBookings = [];
         const currentBookings = [];
         const today = new Date()
@@ -41,28 +43,27 @@ document.addEventListener("DOMContentLoaded", async () => {
 
 
         });
-        // console.log(currentBookings)
 
         const pastBookingsContainer = document.querySelector(".pastBookings");
         const pastBookHtml = pastBookings.map(
             ({ Kitchen: { name, imgPath }, kitchenId, isConfirmed, id }) => {
+                let confirmation = ''
+                let cancelButton = ''
+
                 if(isConfirmed){
-                    return `
-                <div class="past-booking" id="booking-${id}">
-                    <div class="past-booking-kitchen-name"> ${name} </div>
-                    <div class="past-booking-confirmed"> Confirmed! </div>
-                    <a class="details-booking-${id}" href="/bookings/${id}">Details</a>
-                </div>
-                `
+                    confirmation = "Confirmed!"
+                    cancelButton = `<a class="cancel-booking-button" href="/bookings/${id}">Cancel</a>`
                 } else {
-                    return `
+                    confirmation = "Cancelled!"
+                }
+
+                return `
                 <div class="past-booking" id="booking-${id}">
                     <div class="past-booking-kitchen-name"> ${name} </div>
-                    <div class="past-booking-cancelled"> Cancelled! </div>
-                    <a class="details-booking-${id}" href="/bookings/${id}">Details</a>
+                    <div class="past-booking-confirmation"> ${confirmation} </div>
+                    ${cancelButton}
                 </div>
                 `
-                }
                 // Here is the code to add the image, once the images in the database makes sense: <img src="${imgPath[0]}">
             });
         pastBookingsContainer.innerHTML = `<div class="past-booking__header"> Past Bookings </div> ${pastBookHtml.join("")}`;
@@ -70,23 +71,21 @@ document.addEventListener("DOMContentLoaded", async () => {
         const currentBookingsContainer = document.querySelector(".currentBookings");
         const currentBookHtml = currentBookings.map(
             ({ Kitchen: { name, imgPath }, kitchenId, isConfirmed, id }) => {
+                let confirmation = ''
+
                 if(isConfirmed){
-                    return `
-                <div class="current-booking" id="booking-${id}">
-                    <div class="current-booking-kitchen-name"> ${name} </div>
-                    <div class="current-booking-confirmed"> Confirmed! </div>
-                    <a class="details-booking-${id}" href="/bookings/${id}">Details</a>
-                </div>
-                `
+                    confirmation = "Confirmed!"
                 } else {
-                    return `
+                    confirmation = "Cancelled!"
+                }
+
+                return `
                 <div class="current-booking" id="booking-${id}">
                     <div class="current-booking-kitchen-name"> ${name} </div>
-                    <div class="current-booking-cancelled"> Cancelled! </div>
+                    <div class="current-booking-confirmation"> ${confirmation} </div>
                     <a class="details-booking-${id}" href="/bookings/${id}">Details</a>
                 </div>
                 `
-                }
                 // Here is the code to add the image, once the images in the database makes sense: <img src="${imgPath[0]}">
             });
         currentBookingsContainer.innerHTML = `<div class="current-booking__header"> Current Bookings </div> ${currentBookHtml.join("")}`;
